991688ef70a226c12440846f3f09d2636164cf4e
zegabr <jgsp2@cin.ufpe.br>
2021-12-06 22:33:36 -0300

    add thyroid 20

--- SalpSwarm/SalpSwarm/Main.java
+++ SalpSwarm/SalpSwarm/Main.java
@@ -17,8 +17,9 @@ public class Main {
 
     public static void main(String[] args) {
         // run20(new Haberman());
-        //run20(new Cancer());
-        run20(new Liver());
+        // run20(new Cancer());
+        // run20(new Liver());
+        run20(new Thyroid());
     }
 
     public static void run20(ExecutionDetails details) {
